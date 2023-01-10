c37cde8fe6554a462260c64563352036eef6170b
Anzhelika Ryvkina <ryvkinaas@rusagromaslo.com>
2023-01-10 13:15:38 +0500

    task1

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,7 +1,8 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
-
+    let message = productName + " за "+ productPrice + " теперь в корзине!"
+    
     // Задание №1.1. Оповещение о добавлении в корзину
 
     // создайте переменную message
